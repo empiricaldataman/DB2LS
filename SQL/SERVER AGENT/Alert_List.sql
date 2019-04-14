99374486a1f7131bc90fefc59fcd1c5d0b1ced28
Sal Young <salyo@YOGA920.THERIVER.PTY>
2019-04-14 10:20:18 -0500

    First version

---
+++ SQL/SERVER AGENT/Alert_List.sql
@@ -0,0 +1,31 @@
+/*-----------------------------------------------------------------------------------------------
+        NAME: Alert_List.sql
+  CREATED BY: Sal Young
+       EMAIL: saleyoun@yahoo.com
+ DESCRIPTION: Displays details about all the alerts
+-------------------------------------------------------------------------------------------------
+-- TR/PROJ#   DATE        MODIFIED      DESCRIPTION   
+-------------------------------------------------------------------------------------------------
+-- F000000    04.14.2019  SYoung        Initial creation.
+-------------------------------------------------------------------------------------------------
+  DISCLAIMER: The AUTHOR  ASSUMES NO RESPONSIBILITY  FOR ANYTHING, including  the destruction of 
+              personal property, creating singularities, making deep fried chicken, causing your 
+              toilet to  explode, making  your animals spin  around like mad, causing hair loss, 
+              killing your buzz or ANYTHING else that can be thought up.
+-------------------------------------------------------------------------------------------------*/
+SET NOCOUNT ON
+
+SELECT CAST(GETDATE() AS DATE) [date]
+     , @@SERVERNAME [instance_name]
+     , A.[name] [alert]
+     , A.[enabled]
+     , A.[performance_condition]
+     , A.[message_id]
+     , A.[severity]
+     , CASE A.[has_notification] WHEN 1 THEN 'Email' WHEN 2 THEN 'Page' WHEN 3 THEN 'Email/Page' ELSE 'None' END [has_notification]
+     , O.[name] [operator]
+  FROM [msdb].[dbo].[sysalerts] A
+ INNER JOIN [msdb].[dbo].[sysnotifications] N ON A.id = N.alert_id
+ INNER JOIN [msdb].[dbo].[sysoperators] O ON N.operator_id = O.id
+ORDER BY A.[name]
+GO
