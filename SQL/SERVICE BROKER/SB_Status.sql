49b19d506c3d8acd95e96f73cbb68bc54bc1b7c8
Sal Young <salyo@YOGA920.THERIVER.PTY>
2019-04-14 10:18:23 -0500

    Moved and renamed from DBA_ServiceBroker_Status

---
+++ SQL/SERVICE BROKER/SB_Status.sql
@@ -0,0 +1,31 @@
+/*-----------------------------------------------------------------------------------------------
+        NAME: SB_Status.sql
+ MODIFIED BY: Sal Young
+       EMAIL: saleyoun@yahoo.com
+ DESCRIPTION: Displays Service Broker information. It shows if the
+              feature is enabled and if a db master key exist.
+-------------------------------------------------------------------------------------------------
+--  CHANGE HISTORY:
+-- TR/PROJ#    DATE        MODIFIED      DESCRIPTION   
+-------------------------------------------------------------------------------------------------
+-- F000000     02.12.2012  SYoung        Initial creation.
+-------------------------------------------------------------------------------------------------
+  DISCLAIMER: The AUTHOR  ASSUMES NO RESPONSIBILITY  FOR ANYTHING, including  the destruction of 
+              personal property, creating singularities, making deep fried chicken, causing your 
+              toilet to  explode, making  your animals spin  around like mad, causing hair loss, 
+              killing your buzz or ANYTHING else that can be thought up.
+-------------------------------------------------------------------------------------------------*/
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+SET NOCOUNT ON
+GO
+
+USE master
+GO
+
+SELECT [name] [database_name]
+     , is_broker_enabled [is_sb_enabled]
+     , is_master_key_encrypted_by_server [has_db_master_key]
+  FROM sys.databases
+ WHERE is_broker_enabled = 1;
+GO
