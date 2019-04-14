6f36ad2306e4b7844210e3ebb943898309a5825d
Sal Young <salyo@YOGA920.THERIVER.PTY>
2019-04-14 10:19:19 -0500

    Moved to new SQLCMD location

---
+++ SQL/SQLCMD/TYPE.sql
@@ -0,0 +1,21 @@
+/*-------------------------------------------------------------------------------------------------
+        NAME: CMD_TYPE.sql
+  CREATED BY: Sal Young
+       EMAIL: saleyoun@yahoo.com
+ DESCRIPTION: Displays the contents of one or more text files using SQLCMD mode
+-------------------------------------------------------------------------------------------------
+-- TR/PROJ#   DATE        MODIFIED      DESCRIPTION   
+-------------------------------------------------------------------------------------------------
+-- F000000    07.21.2016  SYoung        Initial creation.
+-------------------------------------------------------------------------------------------------
+  DISCLAIMER: The AUTHOR  ASSUMES NO RESPONSIBILITY  FOR ANYTHING, including  the destruction of 
+              personal property, creating singularities, making deep fried chicken, causing your 
+              toilet to  explode, making  your animals spin  around like mad, causing hair loss, 
+              killing your buzz or ANYTHING else that can be thought up.
+-------------------------------------------------------------------------------------------------*/
+:setvar vPATH "'<full_file_path, sysname, File path>'"
+
+:!!type $(vPATH)
+
+
+
