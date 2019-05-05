bcebcba6de7b20689d72c8745cc690f8c0da3d71
Sal Young <salyo@YOGA920.THERIVER.PTY>
2019-05-05 11:25:27 -0500

    adjusted code to display per second values

--- SQL/DATA COLLECTOR/DC_PerfCounter.sql
+++ SQL/DATA COLLECTOR/DC_PerfCounter.sql
@@ -19,15 +19,16 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 SELECT [collection_time]
      , [processes_blocked] - LAG([processes_blocked]) OVER (ORDER BY [collection_time]) [processes_blocked]
      , [user_connections] - LAG([user_connections]) OVER (ORDER BY [collection_time]) [user_connections]
-     , [free_list_stalls_sec] - LAG([free_list_stalls_sec]) OVER (ORDER BY [collection_time]) [free_list_stalls_sec]
-     , [lazy_writes_sec] - LAG([lazy_writes_sec]) OVER (ORDER BY [collection_time]) [lazy_writes_sec]
+     , ([free_list_stalls_sec] - LAG([free_list_stalls_sec]) OVER (ORDER BY [collection_time])) / 60. [free_list_stalls_sec]
+     , ([lazy_writes_sec] - LAG([lazy_writes_sec]) OVER (ORDER BY [collection_time])) / 60. [lazy_writes_sec]
      , [page_life_expectancy] - LAG([page_life_expectancy]) OVER (ORDER BY [collection_time]) [page_life_expectancy]
-     , [full_scans_sec] - LAG([full_scans_sec]) OVER (ORDER BY [collection_time]) [full_scans_sec]
-     , [index_searches_sec] - LAG([index_searches_sec]) OVER (ORDER BY [collection_time]) [index_searches_sec]
-     , [batch_requests_sec] - LAG([batch_requests_sec]) OVER (ORDER BY [collection_time]) [batch_requests_sec]
-     , [sql_compilations_sec] - LAG([sql_compilations_sec]) OVER (ORDER BY [collection_time]) [sql_compilations_sec]
-     , [sql_re-compilations_sec] - LAG([sql_re-compilations_sec]) OVER (ORDER BY [collection_time]) [sql_re-compilations_sec]
+     , ([full_scans_sec] - LAG([full_scans_sec]) OVER (ORDER BY [collection_time])) / 60. [full_scans_sec]
+     , ([index_searches_sec] - LAG([index_searches_sec]) OVER (ORDER BY [collection_time])) / 60. [index_searches_sec]
+     , ([batch_requests_sec] - LAG([batch_requests_sec]) OVER (ORDER BY [collection_time])) / 60. [batch_requests_sec]
+     , ([sql_compilations_sec] - LAG([sql_compilations_sec]) OVER (ORDER BY [collection_time])) / 60. [sql_compilations_sec]
+     , ([sql_re-compilations_sec] - LAG([sql_re-compilations_sec]) OVER (ORDER BY [collection_time])) / 60. [sql_re-compilations_sec]
      , [memory_grants_pending] - LAG([memory_grants_pending]) OVER (ORDER BY [collection_time]) [memory_grants_pending]
   FROM [dbo].[PerfCounter]
  WHERE 1 = 1
  ORDER BY 1 DESC
+
